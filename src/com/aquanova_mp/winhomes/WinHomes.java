ffe69726d5af30c409e671f46cca83cb4095f5df
Unknown <shaad1@live.nl>
2018-01-19 20:14:49 +0100

    Basic setup of plugin

---
+++ src/com/aquanova_mp/winhomes/WinHomes.java
@@ -0,0 +1,15 @@
+package com.aquanova_mp.winhomes;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class WinHomes extends JavaPlugin {
+	@Override
+	public void onEnable() {
+
+	}
+
+	@Override
+	public void onDisable() {
+
+	}
+}
