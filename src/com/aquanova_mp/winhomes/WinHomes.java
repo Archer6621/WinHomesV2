7110bb8a6e44fe1c3adfc440200e1177b318e033
Unknown <shaad1@live.nl>
2018-01-19 20:43:32 +0100

    Add some test messages

--- src/com/aquanova_mp/winhomes/WinHomes.java
+++ src/com/aquanova_mp/winhomes/WinHomes.java
@@ -2,14 +2,16 @@ package com.aquanova_mp.winhomes;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.logging.Level;
+
 public class WinHomes extends JavaPlugin {
 	@Override
 	public void onEnable() {
-
+		getLogger().log(Level.INFO,"Hello World!");
 	}
 
 	@Override
 	public void onDisable() {
-
+		getLogger().log(Level.INFO,"Bye World...");
 	}
 }
