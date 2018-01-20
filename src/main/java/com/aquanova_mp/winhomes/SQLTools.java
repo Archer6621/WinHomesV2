49335a9031636963c4c2938761b02a7aa18aa216
Unknown <shaad1@live.nl>
2018-01-20 15:36:03 +0100

    Add query substitution

--- src/main/java/com/aquanova_mp/winhomes/SQLTools.java
+++ src/main/java/com/aquanova_mp/winhomes/SQLTools.java
@@ -4,11 +4,18 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 
 public class SQLTools {
 
-	public static String queryReader(String query) throws IOException {
-		InputStream in = SQLTools.class.getResourceAsStream("/queries/"+query);
+	public static class QueryException extends Exception {
+		public QueryException(String message) {
+			super(message);
+		}
+	}
+
+	public static String queryReader(String queryPath) throws IOException {
+		InputStream in = SQLTools.class.getResourceAsStream("/queries/"+queryPath);
 		BufferedReader br = new BufferedReader(new InputStreamReader(in));
 		StringBuilder str = new StringBuilder();
 		String line;
@@ -17,5 +24,23 @@ public class SQLTools {
 			str.append("\n");
 		}
 		return str.toString();
+	}
+
+	public static String queryReader(String queryPath, ArrayList<String> substitutions) throws IOException, QueryException {
+		String queryString = queryReader(queryPath);
+
+		for (String sub : substitutions) {
+			if (queryString.contains("?")) {
+				queryString = queryString.replaceFirst("\\?", sub);
+			} else {
+				throw new QueryException("Too many substitution parameters given");
+			}
+		}
+
+		if (queryString.contains("?")) {
+			throw new QueryException("Too few substitution parameters given");
+		}
+
+		return queryString;
 	}
 }
--- src/test/java/com/aquanova_mp/winhomes/SQLToolsTest.java
+++ src/test/java/com/aquanova_mp/winhomes/SQLToolsTest.java
@@ -6,6 +6,8 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class SQLToolsTest {
 	@Test
@@ -17,5 +19,27 @@ public class SQLToolsTest {
 			e.printStackTrace();
 		}
 		assertEquals("test1\ntest2\ntest3\n", str);
+	}
+
+	@Test
+	void queryReaderTestSub() {
+		String str = "";
+		try {
+			str = SQLTools.queryReader("testfile_sub.txt", new ArrayList<>(Arrays.asList("aa", "bb", "cc")));
+		} catch (IOException | SQLTools.QueryException e) {
+			System.out.println(e.getMessage());
+			e.printStackTrace();
+		}
+		assertEquals("SELECT aa FROM bb WHERE cc==1;\n", str);
+	}
+
+	@Test
+	void queryReaderTestSubTooMany() {
+		assertThrows(SQLTools.QueryException.class, () -> SQLTools.queryReader("testfile_sub.txt", new ArrayList<>(Arrays.asList("aa", "bb", "cc", "dd"))));
+	}
+
+	@Test
+	void queryReaderTestSubTooFew() {
+		assertThrows(SQLTools.QueryException.class, () -> SQLTools.queryReader("testfile_sub.txt", new ArrayList<>(Arrays.asList("aa", "bb"))));
 	}
 }
