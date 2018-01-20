62e950c5379d25f995663143150991338a1feda6
Unknown <shaad1@live.nl>
2018-01-20 16:24:13 +0100

    Clean-up and change ints to doubles for database

--- src/main/java/com/aquanova_mp/winhomes/SQLTools.java
+++ src/main/java/com/aquanova_mp/winhomes/SQLTools.java
@@ -1,10 +1,17 @@
 package com.aquanova_mp.winhomes;
 
+import com.mysql.jdbc.jdbc2.optional.MysqlDataSource;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
+import java.util.logging.Level;
 
 /**
  * This class contains several tools for interacting with the database and related matters
@@ -64,5 +71,49 @@ public class SQLTools {
 		}
 
 		return queryString;
+	}
+
+	public static void initializeDataBase(WinHomes main, MysqlDataSource dataSource) {
+		try {
+			Connection conn = dataSource.getConnection();
+
+			// Check whether winhomes database already exists
+			ResultSet resultSet = conn.getMetaData().getCatalogs();
+			boolean exists = false;
+			// Iterate through catalog to find the database names
+			while (resultSet.next()) {
+				String databaseName = resultSet.getString(1); // Database name is at the first position
+				main.getLogger().log(Level.INFO,databaseName);
+				if (databaseName.equals("winhomes")) {
+					exists = true;
+					main.getLogger().log(Level.INFO,"winhomes database found, continuing...");
+				}
+			}
+			resultSet.close();
+
+			// Create the database if it was not present, along with the tables
+			Statement stmt = conn.createStatement();
+			if (!exists) {
+				main.getLogger().log(Level.INFO,"winhomes database not found, creating it!");
+
+				stmt.executeUpdate("CREATE DATABASE winhomes;");
+
+				// Load the query with the create statements for the tables
+				String query = SQLTools.queryReader("create_tables.sql");
+				ResultSet rs = stmt.executeQuery(query);
+				rs.close();
+			}
+
+			stmt.close();
+			conn.close();
+			main.getLogger().log(Level.INFO,"Database successfully initialized!");
+
+		} catch (SQLException e) {
+			main.getLogger().log(Level.WARNING, "Could perform database initialization!");
+			main.getLogger().log(Level.WARNING, e.toString());
+		} catch (IOException e) {
+			main.getLogger().log(Level.WARNING, "Could not open initialization query file!");
+			main.getLogger().log(Level.WARNING, e.toString());
+		}
 	}
 }
--- src/main/resources/queries/create_tables.sql
+++ src/main/resources/queries/create_tables.sql
@@ -1,5 +1,5 @@
 -- Created by Vertabelo (http://vertabelo.com)
--- Last modification date: 2018-01-20 03:01:39.154
+-- Last modification date: 2018-01-20 15:12:41.906
 
 -- tables
 -- Table: home
@@ -8,11 +8,11 @@ USE winhomes;
 CREATE TABLE home (
     UUID varchar(36) NOT NULL,
     Name varchar(64) NOT NULL,
-    X int NOT NULL,
-    Y int NOT NULL,
-    Z int NOT NULL,
-    yaw int NOT NULL,
-    pitch int NOT NULL,
+    X double(12,4) NOT NULL,
+    Y double(12,4) NOT NULL,
+    Z double(12,4) NOT NULL,
+    yaw double(12,4) NOT NULL,
+    pitch double(12,4) NOT NULL,
     world varchar(64) NOT NULL,
     CONSTRAINT home_pk PRIMARY KEY (UUID)
 );
@@ -27,7 +27,8 @@ CREATE TABLE home_invite (
 -- foreign keys
 -- Reference: home_invite_home (table: home_invite)
 ALTER TABLE home_invite ADD CONSTRAINT home_invite_home FOREIGN KEY home_invite_home (home_UUID)
-    REFERENCES home (UUID);
+REFERENCES home (UUID);
 
 -- End of file.
+
 
